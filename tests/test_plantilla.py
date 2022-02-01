61391c72ed0ca0e9c36e3ccbc0591a32c490b727
xaxi <frapercan1@alum.us.es>
2022-02-01 20:33:03 +0100

    CI/CD configuracion

--- tests/test_plantilla.py
+++ tests/test_plantilla.py
@@ -1,4 +1,8 @@
+import yaml
+
 from iecasdmx.consulta import Consulta
 
 def test_consulta_positiva():
-    Consulta(49325)
+    with open("iecasdmx/configuracion.yaml", 'r') as fichero_configuracion:
+        configuracion = yaml.safe_load(fichero_configuracion)
+        Consulta(49325,configuracion)
