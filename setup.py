7bb1a206c4110d7707bf51e6fa02ac15413cc849
frapercan <frapercan1@alum.us.es>
2022-07-19 13:36:36 +0200

    Nueva versión estable.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def read(filename):
 
 setup(
     name="IECA-extractor",
-    version="1.0.0",
+    version="1.1.0",
     url="https://github.com/frapercan/IECA-extractor",
     license='MIT',
 
--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,3 @@
 # flake8: noqa
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 __author__ = 'frapercan <frapercan1@alum.us.es>'
